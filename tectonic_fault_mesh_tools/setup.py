4a8690f18fd07ee891b7fe940e656a4d92650d92
andy22b <andy22b@gmail.com>
2023-01-12 10:59:37 +1300

    Add instructions for updating code and update version number

--- tectonic_fault_mesh_tools/setup.py
+++ tectonic_fault_mesh_tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='fault_mesh',
-      version='0.1',
+      version='0.2',
       description='Tools for building fault meshes in leapfrog',
       author='Andy Howell',
       author_email='a.howell@gns.cri.nz',
