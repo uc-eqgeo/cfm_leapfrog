b3b4eff4adc041ceef1662af7f48ebb2bddc2bee
ahowell <a.howell@gns.cri.nz>
2023-08-23 17:52:09 +1200

    Use of IGL to collapse triangles

---
+++ scratch/collapse_tris/test_collapse.py
@@ -0,0 +1,27 @@
+import igl
+import numpy as np
+import scipy
+import glob
+import os.path
+
+min_area = 1.e4
+stls = list(glob.glob("all_remeshed/*.stl"))
+epsilons = np.array([10**x for x in range(-6, -2)])
+
+for stl in stls:
+    v, f = igl.read_triangle_mesh(stl)
+    min_tri_area = (igl.doublearea(v, f) / 2.0).min()
+
+    if min_tri_area < min_area:
+        for eps in epsilons:
+            out = igl.collapse_small_triangles(v, f, eps=eps)
+            min_tri_area_i = (igl.doublearea(v, out) / 2.0).min()
+            if min_tri_area_i > min_area:
+                break
+        igl.write_triangle_mesh("all_collapsed/" + os.path.basename(stl).replace(".stl", "_collapsed.stl"), v, out)
+        print(stl)
+        print(f"Collapsed {len(f) - len(out)} triangles")
+    else:
+        igl.write_triangle_mesh("all_collapsed/" + os.path.basename(stl).replace(".stl", "_collapsed.stl"), v, f)
+
+
